bd4df92f5be4e4a02613e4a72db2c191071a62dd
Eve Ster <paraskevi.stergiopoulou@kcl.ac.uk>
2022-10-12 23:12:53 +0100

    Exercise 1.3 completed

--- primes.py
+++ primes.py
@@ -3,4 +3,17 @@
 
 def primes(number_of_primes):
     list = []
+    num = 1
+    while len(list) < number_of_primes:
+        num = num + 1
+        if isPrime(num):
+            list.append(num)
     return list
+
+def isPrime(num):
+    for i in range(2, num):
+        if num%i == 0:
+            return False
+    return True
+
+
